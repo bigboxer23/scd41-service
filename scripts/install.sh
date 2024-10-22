fdaee885be33d0b24d55071b4dd447ee0a22b1cb
Matt Jones <bigboxer23@gmail.com>
2024-10-22 12:17:30 -0500

    feat: add README.md, license file

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@ ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com"
 ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23"
 ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23/scd41-service"
 ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23/scd41-service/1.0.0"
+mvn package -DskipTests
 
 scp -o StrictHostKeyChecking=no -r scd41-service.service pi@$host:~/
 ssh -t pi@$host -o StrictHostKeyChecking=no "sudo mv ~/scd41-service.service /lib/systemd/system"
