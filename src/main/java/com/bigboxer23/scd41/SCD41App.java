5bc0bdfe176e348a9f51aa26cf45b1df889c3141
Matt Jones <bigboxer23@gmail.com>
2024-10-30 10:45:53 -0500

    feat: adding swagger definition to rest endpoints

--- src/main/java/com/bigboxer23/scd41/SCD41App.java
+++ src/main/java/com/bigboxer23/scd41/SCD41App.java
@@ -1,11 +1,25 @@
 package com.bigboxer23.scd41;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(
+		info =
+				@Info(
+						title = "SCD41 Service",
+						version = "1",
+						description = "An averaged reading of CO2, temperature (C), and relative humidity"
+								+ " (%) from a SCD41 (https://www.adafruit.com/product/5190) ",
+						contact =
+								@Contact(
+										name = "bigboxer23@gmail.com",
+										url = "https://github.com/bigboxer23/scd41-service")))
 public class SCD41App {
 	public static void main(String[] args) {
 		SpringApplication.run(SCD41App.class, args);
--- src/main/java/com/bigboxer23/scd41/WebController.java
+++ src/main/java/com/bigboxer23/scd41/WebController.java
@@ -1,5 +1,10 @@
 package com.bigboxer23.scd41;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.net.HttpURLConnection;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 /** */
 @RestController
+@Tag(
+		name = "SCD41 Data service",
+		description =
+				"APIs available for fetching CO2, Temperature, and Relative Humidity from an" + " underlying sensor")
 public class WebController {
 	private static final Logger logger = LoggerFactory.getLogger(WebController.class);
 
@@ -18,6 +27,13 @@ public class WebController {
 	}
 
 	@GetMapping(path = "/data", produces = "application/json;charset=UTF-8")
+	@Operation(
+			summary = "Get data averaged over the last 30 seconds for CO2, Temperature, Relative" + " Humidity",
+			description = "Map of data is returned (CO2 PPM, Temperature Celcius, Relative Humidity %)")
+	@ApiResponses({
+		@ApiResponse(responseCode = HttpURLConnection.HTTP_BAD_REQUEST + "", description = "Bad request"),
+		@ApiResponse(responseCode = HttpURLConnection.HTTP_OK + "", description = "success")
+	})
 	public Map<String, Float> getAveragedData() {
 		logger.debug("Data requested");
 		return scd41Component.getAveragedData();
